68fd2b43f18670c617779b11b79ad4dad521f756
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2022-11-09 22:13:10 -0500

    add data.clj back

---
+++ test/donut/minimal/data.clj
@@ -0,0 +1,24 @@
+(ns donut.minimal.data
+  (:require [clojure.spec.alpha :as s]
+            [clojure.spec.gen.alpha :as sg]
+            [donut.datapotato.next-jdbc :as dfn]
+            [donut.endpoint.test.harness :as deth]
+            [donut.minimal.backend.system] ;; for multimethod
+            [next.jdbc :as jdbc]))
+
+(defn db-connection
+  []
+  (deth/instance [:db :connection]))
+
+(def potato-schema
+  {:example-entity {:prefix   :ee
+                    :generate {:schema [:map]}
+                    :fixtures {:table-name "example_entity"}}})
+
+(def datapotato-db
+  {:schema   potato-schema
+   :generate {:generator (comp sg/generate s/gen)}
+   :fixtures (merge dfn/config
+                    {:get-connection (fn [_] (db-connection))
+                     :setup          (fn [{{:keys [connection]} :fixtures}]
+                                       (jdbc/execute! connection ["TRUNCATE TABLE user CASCADE"]))})})
