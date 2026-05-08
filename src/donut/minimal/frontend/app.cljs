44436c973fe023c1dddd7846209601f3e43e1b8a
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2026-05-08 10:47:43 -0400

    nicer home page layout

--- src/donut/minimal/frontend/app.cljs
+++ src/donut/minimal/frontend/app.cljs
@@ -7,12 +7,42 @@
 
 (defn app
   []
-  [:div
-   [:div
-    [:main {:class "flex-1"}
-     [:div {:class "max-w-2xl mx-auto"
-            :role  "topnav"}
-      [dnc/simple-route-link {:route-name :home}
-       "home"]]
-     [:div {:class "max-w-2xl mx-auto mt-4"}
-      @(rf/subscribe [::dnf/routed-component :main])]]]])
+  [:div {:class "drawer lg:drawer-open"}
+   [:input {:id "my-drawer-4", :type "checkbox", :class "drawer-toggle"}]
+
+   ;; right-hand section
+   [:div {:class "drawer-content"}
+    [:nav {:class "navbar w-full bg-base-300"}
+     [:label {:for "my-drawer-4", :aria-label "open sidebar", :class "btn btn-square btn-ghost"}
+      [:svg {:xmlns "http://www.w3.org/2000/svg", :viewBox "0 0 24 24", :stroke-linejoin "round", :stroke-linecap "round", :stroke-width "2", :fill "none", :stroke "currentColor", :class "my-1.5 inline-block size-4"}
+       [:path {:d "M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"}]
+       [:path {:d "M9 4v16"}]
+       [:path {:d "M14 10l2 2l-2 2"}]]]
+     [:div {:class "px-4"} "Navbar Title"]]
+    [:div {:class "p-4"}
+     ;; main content
+     [:div
+      @(rf/subscribe [::dnf/routed-component :main])]]]
+
+   ;; side drawer
+   [:div {:class "drawer-side is-drawer-close:overflow-visible"}
+    [:label {:for "my-drawer-4", :aria-label "close sidebar", :class "drawer-overlay"}]
+    [:div {:class "flex min-h-full flex-col items-start bg-base-200 is-drawer-close:w-14 is-drawer-open:w-64"}
+
+     ;; menu
+     [:ul {:class "menu w-full grow"}
+      [:li
+       [dnc/simple-route-link {:class "is-drawer-close:tooltip is-drawer-close:tooltip-right", :data-tip "Homepage"}
+        [:svg {:xmlns "http://www.w3.org/2000/svg", :viewBox "0 0 24 24", :stroke-linejoin "round", :stroke-linecap "round", :stroke-width "2", :fill "none", :stroke "currentColor", :class "my-1.5 inline-block size-4"}
+         [:path {:d "M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"}]
+         [:path {:d "M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"}]]
+        [:span {:class "is-drawer-close:hidden"} "Homepage"]]]
+      #_
+      [:li
+       [:button {:class "is-drawer-close:tooltip is-drawer-close:tooltip-right", :data-tip "Settings"}
+        [:svg {:xmlns "http://www.w3.org/2000/svg", :viewBox "0 0 24 24", :stroke-linejoin "round", :stroke-linecap "round", :stroke-width "2", :fill "none", :stroke "currentColor", :class "my-1.5 inline-block size-4"}
+         [:path {:d "M20 7h-9"}]
+         [:path {:d "M14 17H5"}]
+         [:circle {:cx "17", :cy "17", :r "3"}]
+         [:circle {:cx "7", :cy "7", :r "3"}]]
+        [:span {:class "is-drawer-close:hidden"} "Settings"]]]]]]])
