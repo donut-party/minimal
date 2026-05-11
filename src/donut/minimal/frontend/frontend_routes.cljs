dc5abe201c0ed9e0ffa8294432f062fc6f578f50
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2026-05-11 14:07:34 -0400

    improve frontend routes

--- src/donut/minimal/frontend/frontend_routes.cljs
+++ src/donut/minimal/frontend/frontend_routes.cljs
@@ -1,21 +1,11 @@
 (ns donut.minimal.frontend.frontend-routes
   (:require
-   [reitit.coercion.malli :as rm]
-   [donut.frontend.form.flow :as dff]
+   [donut.frontend.events :as dfe]
    [donut.frontend.routes :as dfr]
    [donut.frontend.sync.flow :as dsf]
-   [donut.minimal.frontend.components.home :as h]
-   [donut.minimal.frontend.components.example-entity :as ee]))
+   [donut.minimal.frontend.components.home :as h]))
 
 (dfr/defroutes routes
   [["/"
     {:name       :home
-     :components {:main [h/component]}
-     :lifecycle  {:enter [[::dsf/get :example-entities]]}}]
-   ["/example-entities/{example_entity/id}"
-    {:name       :example-entity
-     :components {:main [ee/show]}
-     :coercion   rm/coercion
-     :lifecycle  {:enter [[::dff/clear {:donut.form/key [:post :example-entity]}]
-                          [::dsf/get :example-entity {::dsf/rules #{::dsf/merge-route-params}}]]}
-     :parameters {:path [:map [:example_entity/id int?]]}}]])
+     :components {:main [h/component]}}]])
