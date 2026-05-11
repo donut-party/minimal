219352c3dfbc135727d7e900a888b28740a10387
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2026-05-11 14:39:45 -0400

    note about require

--- src/donut/minimal/backend/endpoint_routes.clj
+++ src/donut/minimal/backend/endpoint_routes.clj
@@ -1,6 +1,8 @@
 (ns donut.minimal.backend.endpoint-routes
   (:require
-   ))
+   ;; NOTE this is only here because :require can't be empty and we
+   ;; want :require for generators to work. Feel free to remove
+   clojure.core))
 
 (def routes
   [;; example
